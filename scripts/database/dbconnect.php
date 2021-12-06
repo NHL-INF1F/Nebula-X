242c667328f87096c09dd750a34c6648127750ce
Stefan <stefan.meijer3@student.nhlstenden.com>
2021-12-06 11:40:42 +0100

    Fixed DB connection security

--- scripts/database/dbconnect.php
+++ scripts/database/dbconnect.php
@@ -1,17 +1,11 @@
 <?php
-    // Connect to server (localhost server)
-    $conn = mysqli_connect("127.0.0.1", "nebulax", "P@ssw0rd");
-    // Test the connection
-    if(!$conn)
-    {
-        DIE("Could not connect: " . mysqli_connect_error());
-    }
-            
-    // Select the database for Nebula-X
-    $db = mysqli_select_db($conn, "nebulax");
-    // Assure the database exists
-	if(!$db)
-	{
-		DIE('Can\'t find database');
-	}
-?>
+//Require ENV
+require_once('env.php');
+
+// Connect to server (localhost server)
+$conn = mysqli_connect($_ENV["hostname"], $_ENV["username"], $_ENV["password"], $_ENV["database"]);
+
+// Test the connection
+if (!$conn) {
+    die("Could not connect: " . mysqli_connect_error());
+}
