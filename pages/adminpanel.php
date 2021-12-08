6f6aa245cede7c40b50e34967e8a14b26bc7c460
RubenVanWoudenberg <91067986+RubenVanWoudenberg@users.noreply.github.com>
2021-12-08 16:12:12 +0100

    Begin van de tables in het Beheerderspaneel

--- pages/adminpanel.php
+++ pages/adminpanel.php
@@ -3,25 +3,52 @@
     <head>
         <title>Adminpanel</title>
         <meta charset="UTF-8">
+        <link href="../assets/styles/adminpanel.css" rel="stylesheet">
     </head>
     <body>
         <div>
             <h1>Admin panel</h1>
         </div>
+        <?php
+            // Select queries
+            //$reservationview = mysqli_query("SELECT * FROM reservation", $conn);
+            //$contactview = mysqli_query("SELECT * FROM contact_messsage", $conn);
+            //$Suites = mysqli_query("SELECT * FROM suite", $conn);
+            //$USERS = mysqli_query("SELECT * FROM user", $conn); 
+        ?>
         <div>
-            <h2>Reserveringen</h2>
+            <h2>Reservations</h2>
         </div>
         <div>        
             <?php
-                
+                echo "<table id=tablereserv>";
+                echo "<tr>
+                    <th>ID</th>
+                    <th>User ID</th>
+                    <th>Name user</th>
+                    <th>Suite ID</th>
+                    <th>Suite Name</th>
+                    <th>Start date</th>
+                    <th>End date</th>
+                </tr>";
+                // here comes code for the rest of the table
+                echo "</table>";
             ?>
         </div>
         <div>
-            <h2>Contactberichten</h2>
+            <h2>Contact Messages</h2>
         </div>
         <div>        
             <?php
-                
+                echo "<table id=tablecontact>";
+                echo "<tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Message</th>
+                </tr>";
+                // here comes code for the rest of the table
+                echo "</table>";
             ?>
         </div>
     </body>
