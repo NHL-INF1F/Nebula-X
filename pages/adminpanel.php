707e8071783a69027af3602018467baca89f26d2
RubenVanWoudenberg <91067986+RubenVanWoudenberg@users.noreply.github.com>
2021-12-08 13:32:38 +0100

    Create adminpanel.php
    pagina Adminpaneel aangemaakt

---
+++ pages/adminpanel.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Adminpanel</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <div>
+            <h1>Admin panel</h1>
+        </div>
+        <div>
+            <h2>Reserveringen</h2>
+        </div>
+        <div>        
+            <?php
+                
+            ?>
+        </div>
+        <div>
+            <h2>Contactberichten</h2>
+        </div>
+        <div>        
+            <?php
+                
+            ?>
+        </div>
+    </body>
+</html>
