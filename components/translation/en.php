180852f88084267328ae05a34239b4b51afc8078
JaafarJawadi29 <91067539+JaafarJawadi29@users.noreply.github.com>
2021-12-15 16:25:58 +0100

    updated en.php with translations based on mock-up

--- components/translation/en.php
+++ components/translation/en.php
@@ -1,6 +1,91 @@
 <?php
 //en.php - english language file
-$message['register']='Register now';
-$message['login']='Login';
-$message['about']='About us';
+
+/*Login page translation*/
+$message['login']='LOGIN';
+$message['email']='EMAIL ADDRESS';
+$message['password']='PASSWORD';
+$message['notamember']='NOT A MEMBER?';
+$message['registerinfo']='Registered users are notified about upcoming events and discounts. By registering you also receive a 5% cost bonus for your next booking!';
+$message['registerbutton']='REGISTER';
+
+/*Index page translation*/
+//Header buttons translation
+$message['gallery']='GALLERY';
+$message['aboutus']='ABOUT US';
+$message['rooms']='ROOMS';
+$message['booking']='BOOKING';
+$message['contact']='CONTACT';
+$message['loginregister']='LOGIN/REGISTER';
+//Main page translations
+$message['experience']='A NEW WAY TO EXPERIENCE SPACE AS NEVER BEFORE';
+$message['learnmore']='LEARN MORE';
+$message['instagram']='INSTAGRAM';
+$message['twitter']='TWITTER';
+
+/*About page translations*/
+$message['about']='ABOUT';
+$message['aim']='WHAT WE AIM TO DO';
+$message['aboutusinfo']="We want to give mankind a way to experience space as never done before! Feel what it's like to be in a zero gravity surrounding or what it's like to wake up in space with your beloved ones or as a solo adventurer!";
+
+/*Rooms page translations*/
+//Search translations
+$message['search']='SEARCH';
+$message['layer']='Layer';
+$message['checkin']='Check in date';
+$message['checkout']='Check out date';
+$message['check']='CHECK';
+//Rooms translations
+$message['spacehub']='SPACE HUB';
+$message['spacecenter']='SPACE CENTER';
+$message['commandcenter']='COMMAND CENTER';
+$message['singleroom']='Single room';
+$message['doubleroom']='Double room';
+$message['luxuryroom']='Luxury room';
+$message['1guest']='UP TO 1 GUEST';
+$message['2guests']='UP TO 2 GUESTS';
+$message['3guests']='UP TO 3 GUESTS';
+$message['readmorerooms']='Read more ->';
+$message['bookhere']='BOOK HERE';
+$message['roomtypes']='ROOM TYPES';
+$message['singledeluxe']='Single Deluxe Stations';
+$message['doubletwin']='Double Twin Stations';
+$message['tripleluxury']='Triple Luxury Stations';
+
+/*Booking page translations*/
+$message['bookingtitle']='BOOKING';
+$message['spacehub']='SPACE HUB';
+$message['bookinginfo']='A single room with air conditioning, Shower. 1x single bed, Safe, Small fridge, Coffee and tea facilities, Phone, Television, Bath with bubble system, Blowdryer.';
+$message['availability']='AVAILABILITY';
+
+/*Gallery page translations*/
+$message['gallerytitle']='GALLERY';
+$message['viewmore']='VIEW MORE';
+
+/*Contact page translations*/
+$message['contactustitle']='CONTACT US';
+$message['help']='HOW CAN WE HELP?';
+$message['contactusinfo']='We want to hear from you, please feel free to email us or just speak your mind below, please allow up to 1 business day to respond.';
+$message['contactform']='CONTACT';
+$message['contactformname']='NAME';
+$message['contactformemail']='EMAIL';
+$message['contactformmessage']='MESSAGE';
+$message['sendmessage']='SEND MESSAGE';
+
+/*Footer translations*/
+$message['nebulaxmotto']='Space as never before!';
+//Explore
+$message['explore']='EXPLORE';
+$message['explorehome']='Home';
+$message['exploregallery']='Gallery';
+$message['exploreaboutus']='About us';
+$message['explorebooking']='Booking';
+$message['explorecontact']='Contact';
+//Visit and HQ
+$message['visit']='VISIT';
+$message['hq']='HQ';
+//Legal
+$message['legal']='LEGAL';
+$message['terms']='Terms';
+$message['privacy']='Privacy';
 ?>
