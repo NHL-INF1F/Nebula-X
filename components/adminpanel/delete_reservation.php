481b0e99c86851c11b837fd2b97d4e1f9d634f51
RubenVanWoudenberg <91067986+RubenVanWoudenberg@users.noreply.github.com>
2021-12-22 00:52:31 +0100

    adminpanel update

--- components/adminpanel/delete_reservation.php
+++ components/adminpanel/delete_reservation.php
@@ -10,15 +10,10 @@
       require_once '../../controllers/database/dbconnect.php';
 
       $sql = "DELETE FROM reservation WHERE ID='$_GET[id]'";
-
-      if(mysqli_query($conn, $sql)){
-        echo "Deletion Successful, please wait to be redirected.";
-        header("refresh url=../../pages/adminpanel.php");
-      }
-      else {
-        echo "Something went wrong, please try again later or contact support.";
-        header("refresh url=../../pages/adminpanel.php");
-      }
+      $stmt = mysqli_prepare($conn, $sql) or die(mysqli_error($conn));
+      mysqli_stmt_execute($stmt) or die("Something went wrong, please try again later or contact support.");
+      echo "<p>Deletion successful, please wait to be redirected.</p>";
+      header("refresh:2 url=../../pages/adminpanel.php");
     ?>
   </body>
 </html>
