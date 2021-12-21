a0cc28f75c49cf755f5780ab17216657db0d4b69
BasDekker1 <basdekker00@live.nl>
2021-12-22 00:52:54 +0100

    Index pagina compleet

--- index.php
+++ index.php
@@ -23,98 +23,7 @@ require_once('controllers/database/dbconnect.php');
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Arimo&display=swap%27" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Arimo&family=Bebas+Neue&display=swap%27" rel="stylesheet">
-
-    <style>
-        /* global */
-        ul {
-            list-style-type: none;
-        }
-
-        h1,
-        h2 {
-            font-family: Bebas Neue;
-        }
-
-        p,
-        a {
-            font-family: Arimo;
-        }
-
-        img {
-            max-width: 180px !important;
-        }
-
-        .arrow {
-            font-size: 2em;
-            cursor: pointer;
-        }
-
-        /* Backgrounds */
-        .spaceBackground {
-            background-image: url('assets/img/space.jpg');
-            background-repeat: no-repeat, repeat;
-            background-size: cover;
-        }
-
-        .spaceStation {
-            background-image: url('assets/img/a140a5c1206d3cd334cf1781d0d41d0b.png');
-            background-repeat: no-repeat, repeat;
-            background-size: 500px;
-            background-position: center;
-        }
-
-        .seeThrough {
-            background-color: rgba(0, 0, 0, 0.185);
-        }
-
-        .footerBackground {
-            background-color: #0f2636;
-        }
-
-        /* header */
-        .header {
-            position: fixed;
-        }
-
-        .headerList {
-            display: inline;
-            margin: 6px;
-
-        }
-
-        .headerList a {
-            font-family: Bebas Neue;
-            color: white;
-            text-decoration: none;
-            font-size: 1.5em;
-        }
-
-        .headerList a:hover {
-            border-bottom: 2px solid white;
-        }
-
-        /* content */
-        .midText {
-            max-width: 400px;
-        }
-
-        /* footer */
-        .footer {
-            position: fixed;
-            left: 0;
-            bottom: 0;
-            color: white;
-        }
-
-        .footer li a {
-            color: white;
-            text-decoration: none;
-        }
-
-        .footer li a:hover {
-            border-bottom: 1px solid white;
-        }
-    </style>
+    <link rel="stylesheet" href="assets/styles/index.css">
 </head>
 
 <body>
@@ -122,7 +31,7 @@ require_once('controllers/database/dbconnect.php');
     <div class="container-fluid header">
         <div class="row">
             <div class="col-12 col-md-6 d-flex justify-content-md-start">
-                <img src="assets/img/logoWit.svg" class="img-fluid" alt="Logo">
+                <img src="assets/img/logo/logoWit.svg" class="img-fluid" alt="Logo">
             </div>
             <div class="col-12 col-md-6 d-flex justify-content-md-end">
                 <ul class="p-0 mt-4">
@@ -130,8 +39,8 @@ require_once('controllers/database/dbconnect.php');
                     <li class="headerList"><a href="index.php">About Us</a></li>
                     <li class="headerList"><a href="index.php">Rooms</a></li>
                     <li class="headerList"><a href="index.php">Booking</a></li>
-                    <li class="headerList"><a href="index.php">Contact</a></li>
-                    <li class="headerList"><a href="index.php">Login/Register</a></li>
+                    <li class="headerList"><a href="pages/contact.php">Contact</a></li>
+                    <li class="headerList"><a href="pages/register.php">Login/Register</a></li>
                 </ul>
             </div>
         </div>
@@ -143,9 +52,11 @@ require_once('controllers/database/dbconnect.php');
 
             </div>
             <div class="col-md-6 text-white min-vh-100">
-                <div class="row min-vh-100 d-flex justify-content-start align-items-center seeThrough">
+                <div id="slidebar" class="row min-vh-100 d-flex justify-content-start align-items-center">
                     <div class="col-1">
-                        <span onclick="hide()" class="arrow"><i class="fas fa-chevron-right"></i></span>
+                        <div onclick="hide()">
+                            <span class="arrow"><i class="fas fa-chevron-right"></i></span>
+                        </div>
                     </div>
                     <div class="col-11">
                         <div>
@@ -164,25 +75,25 @@ require_once('controllers/database/dbconnect.php');
     <div class="container-fluid footer">
         <div class="row">
             <div class="col-md-12">
-                <div class="text-center">
-                    <span class="arrow"><i class="fas fa-chevron-up"></i></span>
-                </div>
                 <div>
-                    <img src="assets/img/spacerx-logo.svg" class="img-fluid" alt="Logo">
+                    <span class="SpaceXLogo">EMPOWERED BY</span>
+                    <img src="assets/img/logo/spacerx-logo.svg" class="img-fluid" alt="Logo">
                 </div>
             </div>
         </div>
     </div>
-
-    <!-- <div class="container-fluid footer footerBackground">
+    
+    <div id="slideFooter" class="container-fluid footer">
         <div class="row">
             <div class="col-md-12 text-center">
-                <span class="arrow"><i class="fas fa-chevron-down"></i></span>
+                <div onclick="show()">
+                    <span class="arrow"><i class="fas fa-chevron-down"></i></span>
+               </div>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-3">
-                <img src="assets/img/logoWit.svg" class="img-fluid" alt="Logo">
+                <img src="assets/img/logo/logoWit.svg" class="img-fluid" alt="Logo">
                 <p>Space as never before!</p>
             </div>
             <div class="col-sm-3">
@@ -191,8 +102,8 @@ require_once('controllers/database/dbconnect.php');
                     <li><a href="">About us</a></li>
                     <li><a href="">Rooms</a></li>
                     <li><a href="">Booking</a></li>
-                    <li><a href="">Contact</a></li>
-                    <li><a href="">Login/Register</a></li>
+                    <li><a href="pages/contact.php">Contact</a></li>
+                    <li><a href="pages/register.php">Login/Register</a></li>
                 </ul>
             </div>
             <div class="col-sm-3">
@@ -217,10 +128,11 @@ require_once('controllers/database/dbconnect.php');
         </div>
         <div class="row text-start">
             <div class="col-md-12">
-                <img src="assets/img/spacerx-logo.svg" class="img-fluid" alt="Logo">
+                <span class="SpaceXLogo">EMPOWERED BY</span>
+                <img src="assets/img/logo/spacerx-logo.svg" class="img-fluid" alt="Logo">
             </div>
         </div>
-    </div> -->
+    </div>
 
 </body>
 
